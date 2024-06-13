e17bc24c1b764d698267ba4920071aebd6a84a08
Nagaraju9182 <nagraju.fresher@gmail.com>
2024-06-13 13:51:53 +0530

    added the new files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+  $characterName = "tom";
+  $characterAge = 40;
+
+   echo "There once was a man name $characterName <br>";
+   echo "he was $characterAge years old <br>";
+   echo "He really liked the name $characterName <br>";
+   echo "but didn't like being $characterAge <br>";
+   ?>
+    
+</body>
+</html>
